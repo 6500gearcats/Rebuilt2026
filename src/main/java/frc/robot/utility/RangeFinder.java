36988d836c784d377172b09c323f612e856ce275
SMQuinn2 <simonmitchellquinn@icloud.com>
2026-02-28 16:11:35 -0500

    updated rangefinder values

--- src/main/java/frc/robot/utility/RangeFinder.java
+++ src/main/java/frc/robot/utility/RangeFinder.java
@@ -13,24 +13,22 @@ public class RangeFinder {
   private static InterpolatingDoubleTreeMap m_map = new InterpolatingDoubleTreeMap();
 
   static {
-    m_map.put(1.43, 2.5);
-    m_map.put(1.73, 2.65);
-    m_map.put(1.8, 2.68);
-    m_map.put(2.01, 2.8);
-    m_map.put(2.2, 2.85);
-    m_map.put(2.48, 2.875);
-    m_map.put(2.63, 2.9);
-    m_map.put(2.75, 3.0);
-    m_map.put(2.90, 3.2);
-    m_map.put(3.03, 3.35);
-    m_map.put(3.15, 3.375);
-    m_map.put(3.37, 3.4);
-    m_map.put(3.61, 3.5);
-    m_map.put(3.77, 3.6);
-    m_map.put(3.99, 3.75);
-    m_map.put(4.4, 5.00);
-
-
+    m_map.put(1.4, 2.5);
+    m_map.put(1.75, 2.7);
+    m_map.put(1.95, 2.8);
+    m_map.put(2.25, 2.9);
+    m_map.put(2.43, 3.0);
+    m_map.put(2.62, 3.1);
+    m_map.put(2.86, 3.2);
+    m_map.put(2.93, 3.3);
+    m_map.put(3.05, 3.4);
+    m_map.put(3.23, 3.45);
+    m_map.put(3.46, 3.6);
+    m_map.put(3.66, 3.8);
+    m_map.put(3.84, 3.9);
+    m_map.put(4.04, 4.2);
+    m_map.put(4.13, 4.5);
+    m_map.put(4.26, 5.2);
   }
 
   public static double getShotVelocity(double distance) {
