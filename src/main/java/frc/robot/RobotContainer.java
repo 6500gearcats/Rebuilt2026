5f534fe46e9f13bf3bdaa8fec5cd22844f4c4255
Pranav <pvanparty@gmail.com>
2026-03-02 17:03:31 -0500

    Gunner Controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,67 +5,38 @@
 package frc.robot;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
-import com.fasterxml.jackson.databind.util.Named;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.FollowPathCommand;
 
 import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Rotation3d;
-import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.util.sendable.Sendable;
-import edu.wpi.first.util.sendable.SendableBuilder;
-
 import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
-import static edu.wpi.first.units.Units.derive;
-
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.StandardOpenOption;
 import java.util.Optional;
-import java.util.function.DoubleSupplier;
-import java.util.function.Supplier;
-
-import javax.crypto.ShortBufferException;
-
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.photonvision.simulation.SimCameraProperties;
 
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.NetworkButton;
-import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.Constants.RobotConstants;
 import frc.robot.commands.AlignTurretToHub;
 import frc.robot.commands.MoveTurret;
-import frc.robot.commands.RunHopper;
 import frc.robot.commands.RunIntake;
-import frc.robot.commands.ShootFuel;
 import frc.robot.commands.ShootingSequence;
 import frc.robot.generated.TunerConstants2;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
@@ -80,7 +51,6 @@ import frc.robot.subsystems.vision.limelight.LimelightIO;
 import frc.robot.subsystems.vision.photonvision.PhotonVisionIO;
 import frc.robot.subsystems.vision.photonvision.PhotonVisionSimIO;
 import frc.robot.utility.RangeFinder;
-import frc.robot.utility.ShooterValuesSenable;
 
 /**
  * Central robot wiring for subsystems, commands, and operator bindings.
@@ -100,12 +70,10 @@ public class RobotContainer {
                         .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive
                                                                                  // motors
         private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
-        private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
         private final Telemetry logger = new Telemetry(MaxSpeed);
 
         private final CommandXboxController joystick = new CommandXboxController(0);
-        private final CommandPS4Controller pranav = new CommandPS4Controller(0);
 
         private final CommandXboxController joystick2 = new CommandXboxController(1);
 
@@ -220,83 +188,34 @@ public class RobotContainer {
                 RobotModeTriggers.disabled().whileTrue(
                                 drivetrain.applyRequest(() -> idle).ignoringDisable(true));
 
-                // hopper.setDefaultCommand(new RunCommand(() -> hopper.startAllMotors(1.6,
-                // 1.7), hopper));
-
-                // joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-                // joystick.b().whileTrue(drivetrain.applyRequest(
-                // () -> point.withModuleDirection(
-                // new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
-
-                // LED test controls
-                // joystick.x().onTrue(Commands.runOnce(() -> m_candle.setLedColor(2, 92, 40)));
-                // // gearcat teal!
-                // joystick.y().onTrue(Commands.runOnce(() -> m_candle.setRainbowAnimation()));
-                // joystick.y().onTrue(new IntakeFuel(m_intake, 1));
-                // joystick.pov(0).whileTrue(new FeedFuel(m_feeder));
-                // joystick.pov(90).whileTrue(new ShootFuel(m_shooter, 1));
-                // joystick.pov(90).whileTrue(new IntakeFuel(m_intake, 1));
-                // joystick.rightBumper().whileTrue(new RunCommand(() ->
-                // m_candle.colorWithBrightness(
-                // Math.sqrt(Math.pow(joystick.getLeftX(), 2) + Math.pow(joystick.getLeftY(),
-                // 2))
-                // )));
-
-                // joystick.leftTrigger().whileTrue(new RunCommand(() ->
-                // m_candle.colorWithBrightness(
-                // joystick.getLeftTriggerAxis()
-                // )));
-
-                // new Trigger(() -> joystick2.getLeftTriggerAxis() > 0.01)
-                // .whileTrue(new RunCommand(() -> m_candle.colorWithBrightness(
-                // () -> joystick2.getLeftTriggerAxis())));
-
-                // Change input str to
-                // joystick.rightBumper().onTrue(Commands.runOnce(() -> m_candle.cycleFlag()));
-
-                // Run SysId routines when holding back/start and X/Y.
-                // Note that each routine should be run exactly once in a single log.
-                // joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-                // joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-                // joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-                // joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
-
-                // joystick.a().whileTrue(drivetrain.applyRequest(() ->
-                // drive.withRotationalRate(getAlignRate() * 0.1)));
-
-                // reset the field-centric heading on left bumper press
-                // joystick.leftBumper().onTrue(drivetrain.runOnce(() ->
-                // drivetrain.seedFieldCentric()));
-
                 // Reset the field-centric heading on left bumper press.
                 joystick.start().onTrue(new InstantCommand(() -> setRobotOrientation()));
-
-                new Trigger(() -> Math.abs(joystick2.getRightX()) > 0.1)
-                                .whileTrue(new MoveTurret(m_turret, () -> joystick2.getRightX() * 0.2));
-
-                joystick.povRight().whileTrue(new MoveTurret(m_turret, () -> 0.2));
-                joystick.povLeft().whileTrue(new MoveTurret(m_turret, () -> -0.2));
 
                 // joystick.rightBumper().onTrue(new RunHopper(hopper));
                 joystick.rightBumper().whileTrue(new RunCommand(() -> m_intake.deployIntake(-0.3)));
                 joystick.leftBumper().whileTrue(new RunIntake(m_intake, -3));
 
-                new Trigger(() -> Math.abs(joystick.getLeftTriggerAxis()) > 0.1)
+                // Gunner
+                new Trigger(() -> Math.abs(joystick2.getLeftTriggerAxis()) > 0.1)
                                 .whileTrue(new ShootingSequence(hopper, m_flywheel, m_turret));
 
-                joystick.y().onTrue(new InstantCommand(() -> m_turret.zeroMotorPosition()));
-                joystick.back().onTrue(new InstantCommand(() -> m_turret.toggleOverride()))
+                joystick2.start().onTrue(new InstantCommand(() -> m_turret.zeroMotorPosition()));
+
+                joystick2.povRight()
+                                .onTrue(new InstantCommand(() -> m_turret.toggleOverride()))
+                                .whileTrue(new MoveTurret(m_turret, () -> 0.2))
                                 .onFalse(new InstantCommand(() -> m_turret.toggleOverride()));
 
-                // joystick.a().onTrue(new InstantCommand(() -> {
-                // ShooterValuesSenable data = (ShooterValuesSenable)
-                // SmartDashboard.getData("Shooter Values");
-                // if (data != null) {
-                // LogValues(data.getDist(), data.getShooterSpeed());
-                // }
+                joystick2.povLeft()
+                                .onTrue(new InstantCommand(() -> m_turret.toggleOverride()))
+                                .whileTrue(new MoveTurret(m_turret, () -> -0.2))
+                                .onFalse(new InstantCommand(() -> m_turret.toggleOverride()));
 
-                // }));
-                joystick.a().whileTrue(new AlignTurretToHub(m_turret));
+                // Old override commmand
+                // joystick2.back().onTrue(new InstantCommand(() -> m_turret.toggleOverride()))
+                // .onFalse(new InstantCommand(() -> m_turret.toggleOverride()));
+
+                joystick2.a().whileTrue(new AlignTurretToHub(m_turret));
         }
 
         /**
