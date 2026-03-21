24d30c43d5c9c99f95f519de814ab5b6721ba9ce
Gearcats6500Programming <gearcats6500programming@gmail.com>
2026-03-21 08:29:31 -0400

    Autos for Wake

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -175,6 +175,9 @@ public class RobotContainer {
                 NamedCommands.registerCommand("NewShootFuel5s",
                                 new ShootingSequenceUTS(hopper, m_flywheel, m_turret, robotStateMachine)
                                                 .withTimeout(5.0));
+                NamedCommands.registerCommand("NewShootFuel10s",
+                                new ShootingSequenceUTS(hopper, m_flywheel, m_turret, robotStateMachine)
+                                                .withTimeout(10.0));
                 NamedCommands.registerCommand("AlignTurret", new AlignTurretToHub(m_turret));
                 NamedCommands.registerCommand("AlignTurret1s", new AlignTurretToHub(m_turret).withTimeout(1));
                 NamedCommands.registerCommand("Climb", new ClimbPole(m_climber, 0.1)); // TODO: set auto speed
