d35c5c04cbb2e9d7ec6ba60c13097423a6f0231a
Gearcats6500Programming <gearcats6500programming@gmail.com>
2026-03-30 19:46:31 -0400

    Changes to deadline group for shoot

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -133,7 +133,7 @@ public class RobotContainer {
                 joystick = robotStateMachine.getDriver();
                 m_gunner = robotStateMachine.getGunner();
                 NamedCommands.registerCommand("AlignTurretFromRightTrench",
-                                new RunCommand(() -> m_turret.setPosition(90), m_turret));
+                                new RunCommand(() -> m_turret.setPosition(-95), m_turret));
                 NamedCommands.registerCommand("IntakeFuel", new RunIntake(m_intake, -1));
                 NamedCommands.registerCommand("IntakeFuelJason", new RunIntake(m_intake, -1).withTimeout(5));
                 NamedCommands.registerCommand("Intake", new RunIntake(m_intake, -0.1).withTimeout(0.2));
