1739aa5a5c3329bfae0201ada08e8f0efea8100f
mquinn <mquinn@xby2.com>
2026-09-07 18:44:01 -0400

    // TEMPORARY TEST OVERRIDE: run the hopper regardless of turret alignment,

--- src/main/java/frc/robot/commands/UpToSpeedHopperShoot.java
+++ src/main/java/frc/robot/commands/UpToSpeedHopperShoot.java
@@ -34,17 +34,18 @@ public class UpToSpeedHopperShoot extends Command {
     if ((!stateMachine.isActive()) && (stateMachine.checkZone() == FieldZone.ALLIANCE)) {
       return;
     }
-    if (SmartDashboard.getBoolean("Aligned", true)) {
-      if (stateMachine.isFarEnough()) {
-        if (m_Flywheel.isUpToSpeed()) {
-          m_Hopper.startAllMotors(-1, 1);
-        }
-      } else {
-        m_Hopper.startAllMotors(-1, 1);
-      }
-
-      // m_Hopper.startAllMotors(-1, 1);
-    }
+    // TEMPORARY TEST OVERRIDE: run the hopper regardless of turret alignment,
+    // distance, or flywheel-speed readiness. Restore the gates below after testing.
+    // if (SmartDashboard.getBoolean("Aligned", true)) {
+    //   if (stateMachine.isFarEnough()) {
+    //     if (m_Flywheel.isUpToSpeed()) {
+    //       m_Hopper.startAllMotors(-1, 1);
+    //     }
+    //   } else {
+    //     m_Hopper.startAllMotors(-1, 1);
+    //   }
+    // }
+    m_Hopper.startAllMotors(-1, 1);
 
   }
 
